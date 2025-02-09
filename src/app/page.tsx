2af2208b9b4060d1ed80530588e321c49f3fbfa0
scifisatan <brawlershere@gmail.com>
2025-02-09 21:28:30 +0545

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-bold text-wrap">Hiii</h1>
+      <Button>Hiii</Button>
+    </>
+  );
+}
